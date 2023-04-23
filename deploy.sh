4d6006665fa8725d8b9c680faa2eebe785f48b86
Frisia-coder <93887912+Frisia-coder@users.noreply.github.com>
2023-04-23 13:11:10 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Upload files to server
-ssh USER@IP_ADDRESS "mkdir -p /var/www/myapp"
-rsync -avz --delete --exclude '.git*' ./ USER@IP_ADDRESS:/var/www/myapp/
+ssh Frisia-coder@104.248.95.135 "mkdir -p /var/www/myapp"
+rsync -avz --delete --exclude '.git*' ./ Frisia-coder@104.248.95.135:/var/www/myapp/
 
 # Restart server
-ssh USER@IP_ADDRESS "sudo service myapp restart"
+ssh Frisia-coder@104.248.95.135 "sudo service myapp restart"
 
 # Starts pytest for myapp file
 pytest --cov=myapp tests/
