c9dae6d9618176c117f0cf6aa70af3d403033cab
Michael <93887912+Frisia-coder@users.noreply.github.com>
2023-04-24 20:57:30 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Upload files to server
-ssh BackEnd@104.248.95.135 "mkdir -p /var/www/myapp"
-rsync -avz --delete --exclude '.git*' ./ BackEnd@104.248.95.135:/var/www/myapp/
+ssh Frisia-coder@104.248.95.135 "mkdir -p /var/www/myapp"
+rsync -avz --delete --exclude '.git*' ./ Frisia-coder@104.248.95.135:/var/www/myapp/
 
 # Restart server
-ssh BackEnd@104.248.95.135 "sudo service myapp restart"
+ssh Frisia-coder@104.248.95.135 "sudo service myapp restart"
