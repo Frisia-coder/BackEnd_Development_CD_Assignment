736bcf632bd8faa5bf5a270426d33929f1439005
Michael <93887912+Frisia-coder@users.noreply.github.com>
2023-04-24 20:55:56 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Upload files to server
-ssh BackEnd-CD@104.248.95.135 "mkdir -p /var/www/myapp"
-rsync -avz --delete --exclude '.git*' ./ BackEnd-CD@104.248.95.135:/var/www/myapp/
+ssh BackEnd@104.248.95.135 "mkdir -p /var/www/myapp"
+rsync -avz --delete --exclude '.git*' ./ BackEnd@104.248.95.135:/var/www/myapp/
 
 # Restart server
-ssh BackEnd-CD@104.248.95.135 "sudo service myapp restart"
+ssh BackEnd@104.248.95.135 "sudo service myapp restart"
