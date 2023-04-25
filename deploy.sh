5fa006212ef1c109d755d8fc974aaea639032a8c
Michael <93887912+Frisia-coder@users.noreply.github.com>
2023-04-25 23:29:28 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Upload files to server
-ssh root@104.248.95.135 "mkdir -p /var/www/myapp"
+ssh root@104.248.95.135 "sudo mkdir -p /var/www/myapp"
 rsync -avz --delete --exclude '.git*' ./ root@104.248.95.135:/var/www/myapp/
 
 # Restart server
