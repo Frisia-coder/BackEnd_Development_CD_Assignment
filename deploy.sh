22e0125f02b92875c3c6cda1e7f2664ad7804176
Frisia-coder <m-kijf@hotmail.nl>
2023-04-30 10:30:21 +0200

    Update Workflow

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Upload files to server
-ssh root@104.248.95.135 "mkdir -p /var/www/myapp"
-rsync -avz --delete --exclude '.git*' ./ root@104.248.95.135:/var/www/myapp/
-
-# Restart server
-ssh root@104.248.95.135 "sudo service myapp restart"
